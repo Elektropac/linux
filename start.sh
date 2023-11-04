c4a6d3d39dedc7fa576dd3f63246f4392e6169f6
ElektroPac <Elektropac@gmail.com>
2023-11-04 23:09:50 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,32 +5,51 @@ while true; do
     clear
     echo -e "Hello Again, nice to see you.\n"
     echo "Select an option:"
-    echo "1. Change Hostname "
-    echo "3"
-    echo "4"
+    echo "1. Install software. "
+    echo "2. Setup Machine. "
+    echo "3. Linux terminal help. "
     echo "5"
     echo "6"
     echo "7"
     echo "8"
     
     
-    echo -e "\n9. Reboot System"
-    echo "10. Exit"
+    echo -e "\n8. Reboot System"
+    echo -e "9. Exit\n"
 
     # Read the user's choice
-    read -p "Enter your choice (1/2/3): " choice
+    read -p "Enter your choice (1/2/3..): " choice
 
     # Process the user's choice
     case $choice in
         1)
+            ./installSoftware.sh
             # Change hostname
-            ./change-hostname.sh
+            #./change-hostname.sh
             ;;
         2)
+            ./setupMachine.sh
+            ;;
+        3)
+            echo "No ready yet"
+            ;;
+        4)
+            echo "No ready yet"
+            ;;
+        5)
+            echo "No ready yet"
+            ;;
+        6)
+            echo "No ready yet"
+            ;;
+        7)
+            echo "No ready yet"
+            ;;
+        8)
             # Reboot system
             sudo reboot
             ;;
-        3)
+        9)
             # Exit the script
             echo "Exiting the script."
             exit 0
