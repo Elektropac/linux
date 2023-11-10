60d56e81cb7e8cc87f9bce52c0f2ec574727c623
ElektroPac <43667224+Elektropac@users.noreply.github.com>
2023-11-10 21:51:59 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,10 +10,10 @@ fi
 declare -A scripts
 scripts["script1.sh"]="Install software"
 scripts["script2.sh"]="Setup"
-scripts["exit"]="Exit"
+scripts["runthis.sh"]="Exit"
 
 # Opret dialog kommando
-cmd=(dialog --separate-output --checklist "Vælg scripts at køre:" 22 76 16)
+cmd=(dialog --separate-output --checklist "What do you want to do:" 22 50 36)
 
 # Tilføj scripts til dialog kommando
 options=()
@@ -24,9 +24,19 @@ done
 # Vis dialogen og gem output
 valgte_scripts=()
 if valgte_scripts=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty); then
+    ordered_scripts=()
     for script in $valgte_scripts; do
-        echo "Kører $script..."
-        ./$script
+        ordered_scripts+=($script)
+    done
+
+    # Define the order in which scripts should be run
+    script_order=("script1.sh" "script2.sh" "runthis.sh")
+
+    for script_to_run in "${script_order[@]}"; do
+        if [[ "${ordered_scripts[@]}" =~ "$script_to_run" ]]; then
+            echo "Kører $script_to_run..."
+            ./$script_to_run
+        fi
     done
 else
     echo "Ingen scripts valgt."
@@ -34,5 +44,3 @@ fi
 
 # Ryd op
 clear
-
-echo "Alle valgte scripts er kørt."
