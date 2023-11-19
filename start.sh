d542cd2386d7fd68b8d98952305b0b80c7359dc5
ElektroPac <43667224+Elektropac@users.noreply.github.com>
2023-11-19 21:43:28 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ echo "What do you like to do? :"
 echo "1. Install software"
 echo "2. Setup terminal"
 echo "3. Manage user"
-echo "4. Exit"
+echo "9. Exit"
 read -p "Enter choise (1-4): " ACTION
 
 case $ACTION in
@@ -24,8 +24,8 @@ case $ACTION in
       ./manage-user.sh
     ;;
 
-    4)
-
+    9)
+      exit
     ;;
 
     esac
