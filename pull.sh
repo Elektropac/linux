d09cef50abd14a0d8ea6171f7f3f72c192860853
ElektroPac <43667224+Elektropac@users.noreply.github.com>
2023-11-19 21:59:05 +0100

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~
+
+echo "#!/bin/bash" > pullgit.sh
+
+echo "sudo rm -r linux" >> pullgit.sh
+
+echo "git clone https://github.com/elektropac/linux" >> pullgit.sh
+echo "sudo chmod 777 linux/*" >> pullgit.sh
+echo "cd linux" >> pullgit.sh
+echo "./start.sh" >> pullgit.sh
+
+
+#sudo rm -r linux
+#git clone https://github.com/elektropac/linux
+#sudo chmod 777 linux/*
+#cd linux
+#./start.sh
